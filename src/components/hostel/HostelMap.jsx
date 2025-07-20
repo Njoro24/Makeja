3931de3584987ee388edb431a762c40a62ddb48c
Ianmwendwa11 <ian.musyoki12@gmail.com>
2025-07-20 14:32:16 +0300

    Update HostelMap.jsx

--- src/components/hostel/HostelMap.jsx
+++ src/components/hostel/HostelMap.jsx
@@ -1,11 +1,1 @@
-import React from 'react';
 
-const HostelMap = () => {
-  return (
-    <div className="w-full h-96 bg-gray-200 rounded-2xl flex items-center justify-center">
-      <p className="text-gray-600">[Map Placeholder]</p>
-    </div>
-  );
-};
-
-export default HostelMap;
