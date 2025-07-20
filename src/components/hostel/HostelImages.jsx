e3dc4a8b28369ec64067effad556bcbe0830de30
Ianmwendwa11 <ian.musyoki12@gmail.com>
2025-07-20 14:43:55 +0300

    Update HostelImages.jsx

--- src/components/hostel/HostelImages.jsx
+++ src/components/hostel/HostelImages.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React from 'react';
 
-const HostelImages = ({ images, name }) => {
+const HostelImages = ({ images }) => {
   return (
-    <img
-      src={images[0]} // Use first image as preview
-      alt={`${name} preview`}
-      className="w-full h-52 object-cover rounded-md border border-gray-700"
-    />
+    <div className="max-w-4xl mx-auto mt-8">
+      <h2 className="text-2xl font-semibold text-purple-800 mb-4">Gallery</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {images.map((img, i) => (
+          <img
+            key={i}
+            src={img}
+            alt={`Gallery ${i+1}`}
+            className="rounded-lg shadow-sm object-cover h-56 w-full hover:scale-105 transition"
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
