7b4c5966816dc2bd281273b5bc94a4b46a8421b9
Ianmwendwa11 <ian.musyoki12@gmail.com>
2025-07-20 14:33:25 +0300

    Update HostelList.jsx

--- src/components/hostel/HostelList.jsx
+++ src/components/hostel/HostelList.jsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import HostelCard from './HostelCard';
 
-const HostelList = ({ hostels, onSelect }) => {
-  return (
-    <div className="flex flex-wrap justify-center">
-      {hostels.map((hostel) => (
-        <HostelCard key={hostel.id} hostel={hostel} onClick={() => onSelect(hostel)} />
-      ))}
-    </div>
-  );
-};
-
-export default HostelList
